4abefa43e22214f6baf7ad5efc4ca20fc4c545c1
ZJYH <1378286199@qq.com>
2020-11-16 20:00:28 +0800

    Update HelloService.java

--- service/HelloService.java
+++ service/HelloService.java
@@ -19,12 +19,15 @@ public class HelloService {
         Car car = new Car();
         car.setBrand("naike");
         car.setBuyDate(new Date());
-        log.info("奥迪撒大苏打car:{}",car);
+        log.info("奥迪撒大苏打car:{}撒旦啊顶顶顶",car);
         car.setCarNum(ProNum.getCarNum(car.getBrand()));
-        skadjffsasfasdf
         car.setBal(car.getCarNum()+1);
         if(car.getBal != null){
             car.setBal =100000000;
+        }
+       
+        for(int i =1; i<10; i++){
+            Ssytem.out.println(i);
         }
         return car;
     }
