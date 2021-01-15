908f3f3c4bba1c4de7c974336d822948f418ad5b
ZH <1378286199@qq.com>
2021-01-15 20:30:05 +0800

    像你一样

--- service/UserService.java
+++ service/UserService.java
@@ -3,10 +3,11 @@ public interface UserService{
     int insOne(); 
     sting getNice();
     sting getName();
-    int 暗示();
-    int modiyOne();
+    int 暗示asdas();
+    int modiyOneasdasddas();
     冷漠凄清又惆怅
     她是有丁香一样的颜色。
-    丁香一样的芬芳。
+    丁香一样的芬芳，像你一样像你一样的。
+
 
 }
