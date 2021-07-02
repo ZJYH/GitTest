3fda2507cc4acde308cce948261a231fb914b83d
张浩 <zhanghao-it@bestpay.com.cn>
2021-07-02 11:26:40 +0800

    测试去除版本控制

---
+++ User.java
@@ -0,0 +1,23 @@
+public class User{
+    
+    
+    private String age;
+    private String good;
+    private String nice;
+    
+    
+    
+    public String getNackName(){
+        return this.name;
+    }
+    
+
+public String getAge£¨£©{
+}
+
+
+public String getGood(){
+        return this.good;
+    }
+    
+}
